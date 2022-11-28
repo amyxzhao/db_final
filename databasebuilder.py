37381dee9163999b363a9d6262738fc84ca29fdf
Amy Zhao <amy.zhao.axz4@yale.edu>
2022-11-28 08:58:24 -0500

    Working demand tables in db

--- databasebuilder.py
+++ databasebuilder.py
@@ -9,24 +9,39 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
 def format_crosslistings(course_data):
-    print("Working")
-
-def find_corresonding_course_name(course_data, id):
+    dict_format = []
 
     with open(course_data, 'r') as csvfile:
         all_courses = csv.reader(csvfile)
         for row in all_courses:
-            if id == row[0]:
-                return row[2]
+            if not any(d['courseid'] == row[0] for d in dict_format):
+                new_dict = {}
+                new_dict["courseid"] = row[0]
+                full_code = row[1]
+                new_dict["subjectcode"] = full_code[:-3]
+                new_dict["coursetitle"] = row[2]
+                dict_format.append(new_dict)
+            else:
+                for existing in dict_format:
+                    if existing["courseid"] == row[0]:
+                        full_code = row[1]
+                        sub_code = full_code[:-3]
+                        existing["subjectcode"] = existing["subjectcode"] + " | " + sub_code
+
+    return dict_format
+
+def find_corresonding_course_name(dict_course_data, id):
+
+    for course in dict_course_data:
+        if course["courseid"] == id:
+            return course["coursetitle"]
 
 
-def find_corresonding_course_code(course_data, id):
+def find_corresonding_subject_code(dict_course_data, id):
 
-    with open(course_data, 'r') as csvfile:
-        all_courses = csv.reader(csvfile)
-        for row in all_courses:
-            if id == row[0]:
-                return row[1]
+    for course in dict_course_data:
+        if course["courseid"] == id:
+            return course["subjectcode"]
 
 def populate_fall_courses(course_data, sql_session):
     print("Working")
@@ -40,16 +55,17 @@ def populate_fall_demand(course_data, demand_data, sql_session, dates):
         all_demand = csv.reader(csvfile)
         all_demand = list(all_demand)
 
+        counter = 0
+
         for i in range(dates + 1, demand_length, dates):
             if i < demand_length:
                 current_course = all_demand[i]
-                code = find_corresonding_course_code(course_data, current_course[0])
-                subject_code = code[:-3]
-                section = code[-2:]
+                subject_code = find_corresonding_subject_code(course_data, current_course[0])
                 title = find_corresonding_course_name(course_data, current_course[0])
-                print(current_course, subject_code, title)
-                new_demand = FallDemand(courseid=current_course[0], coursecode=subject_code, section=section, coursetitle=title, coursedemand=current_course[2])
+                # print(current_course, subject_code, title)
+                new_demand = FallDemand(courseid=current_course[0], coursecode=subject_code, coursetitle=title, coursedemand=current_course[2])
                 sql_session.add(new_demand)
+                counter += 1
     
     sql_session.commit()
 
@@ -64,14 +80,17 @@ def populate_spring_demand(course_data, demand_data, sql_session, dates):
         all_demand = csv.reader(csvfile)
         all_demand = list(all_demand)
 
+        counter = 0
+
         for i in range(dates + 1, demand_length, dates):
-            current_course = all_demand[i]
-            code = find_corresonding_course_code(course_data, current_course[0])
-            subject_code = code[:-3]
-            section = code[-2:]
-            title = find_corresonding_course_name(course_data, current_course[0])
-            new_demand = SpringDemand(courseid=current_course[0], coursecode=subject_code, section=section, coursetitle=title, coursedemand=current_course[2])
-            sql_session.add(new_demand)
+            if i < demand_length:
+                current_course = all_demand[i]
+                subject_code = find_corresonding_subject_code(course_data, current_course[0])
+                title = find_corresonding_course_name(course_data, current_course[0])
+                # print(current_course, subject_code, title)
+                new_demand = SpringDemand(courseid=current_course[0], coursecode=subject_code, coursetitle=title, coursedemand=current_course[2])
+                sql_session.add(new_demand)
+                counter += 1
     
     sql_session.commit()
 
@@ -89,12 +108,15 @@ if __name__ == "__main__":
     spring_courses = "/Users/zhaoamyx/Desktop/CPSC437/Final/db_final/courses_spring23.csv"
     spring_demand = "/Users/zhaoamyx/Desktop/CPSC437/Final/db_final/demand_spring23.csv"
 
+    formatted_fall_courses = format_crosslistings(fall_courses)
+    formatted_spring_courses = format_crosslistings(spring_courses)
+
     Session = sessionmaker(bind=engine)
     session = Session()
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
-    populate_fall_demand(fall_courses, fall_demand, session, 7)
-    populate_spring_demand(spring_courses, spring_demand, session, 8)
+    populate_fall_demand(formatted_fall_courses, fall_demand, session, 7)
+    populate_spring_demand(formatted_spring_courses, spring_demand, session, 8)
 
     print("Done")
--- table.py
+++ table.py
@@ -31,17 +31,16 @@ class SpringCourses(Base):
 class FallDemand(Base):
     """demand statistics table for fall 2022"""
     __tablename__ = 'falldemand'
-    courseid = Column(Text)
-    coursecode = Column(Text, primary_key=True)
-    section = Column(Text)
+    # dummy = Column(Integer, primary_key=True)
+    courseid = Column(Text, primary_key=True)
+    coursecode = Column(Text)
     coursetitle = Column(Text)
     coursedemand = Column(Integer)
 
 class SpringDemand(Base):
     """demand statistics table for spring 2023"""
     __tablename__ = 'springdemand'
-    courseid = Column(Text)
-    coursecode = Column(Text, primary_key=True)
-    section = Column(Text)
+    courseid = Column(Text, primary_key=True)
+    coursecode = Column(Text)
     coursetitle = Column(Text)
     coursedemand = Column(Integer)
