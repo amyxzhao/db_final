e7c27835df040fd6e3fe002ce4dbcd47adb3bae0
Amy Zhao <amy.zhao.axz4@yale.edu>
2022-12-07 08:52:43 -0500

    Complex SQL

--- get_recommendations.py
+++ get_recommendations.py
@@ -1,10 +1,11 @@
 from contextlib import closing
+from recommendations import Recommendation
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 from sqlite3 import connect
 
 
-DB_PATH = 'file:database.sqlite?mode=ro'
+DB_PATH = 'file:database.sqlite'
 
 def get_course_descriptions():
     '''
@@ -64,8 +65,6 @@ def get_coursetitle(courseid):
     
     '''
 
-    # TODO: Address the crosslisting issue!
-
     with connect(DB_PATH, uri=True) as connection:
         with closing(connection.cursor()) as cursor:
             query_string = "SELECT fullcode, title from springcourses WHERE courseid=?"
@@ -117,23 +116,80 @@ def get_recommendations(coursetitle, cosine_sim):
         Returns:
             course_names: List of dictionaries that represent the most similar courses to the one provided. 
     '''
-
-    # TODO: Address the crosslisting issue!
     idx = get_courseid(coursetitle)
     sim_scores = list(enumerate(cosine_sim[idx]))
 
     sim_scores.sort(key=lambda x: x[1], reverse=True)
 
+    # Top 20 matching courses. 
     top_scores = sim_scores[1:50]
-    top_courses_idx = [i[0] for i in top_scores]
-    course_names = []
-    for courseid in top_courses_idx:
-        # A small, albeit not very good fix for crosslistings.
-        c_dict = get_coursetitle(courseid)
-        if not any(c["coursetitle"] == c_dict["coursetitle"] for c in course_names):
-            course_names.append(c_dict)
 
+    course_names = []
+    for course in top_scores:
+        c_dict = {}
+        c_dict["courseid"], c_dict["similarity_score"] = course[0], course[1]
+        course_names.append(c_dict)
+
+    print("Most similar course id: ", course_names[0])
     return course_names[0:10]
+
+def build_rec_table(course_names):
+    
+    with connect(DB_PATH, uri=True) as connection:
+        with closing(connection.cursor()) as cursor:
+
+            clean_up_query = "DROP TABLE IF EXISTS courserecs"
+            cursor.execute(clean_up_query)
+
+            create_base_query = "CREATE TABLE courserecs (courseid INTEGER PRIMARY KEY, similarity NUMERIC(3, 5))"
+            cursor.execute(create_base_query)
+
+            for c in course_names:
+                insert_query = f"INSERT INTO courserecs VALUES ({c['courseid']}, {c['similarity_score']})"
+                cursor.execute(insert_query)
+
+def join_and_update_recs():
+
+    with connect(DB_PATH, uri=True) as connection:
+        with closing(connection.cursor()) as cursor:
+
+            sort_similarity_query = "SELECT c.courseid, s.fullcode, s.title, s.description, d.coursedemand, c.similarity FROM courserecs c LEFT JOIN springcourses s ON c.courseid = s.courseid LEFT JOIN springdemand d ON s.courseid = d.courseid ORDER BY similarity DESC"
+            cursor.execute(sort_similarity_query)
+
+            sorted_by_similarity = cursor.fetchall()
+
+            sort_demand_query = "SELECT c.courseid, s.fullcode, s.title, s.description, d.coursedemand, c.similarity FROM courserecs c LEFT JOIN springcourses s ON c.courseid = s.courseid LEFT JOIN springdemand d ON s.courseid = d.courseid ORDER BY coursedemand DESC"
+            cursor.execute(sort_demand_query)
+
+            sorted_by_demand = cursor.fetchall()
+
+            overall_avg_query = "SELECT AVG(d.coursedemand) FROM courserecs c LEFT JOIN springcourses s ON c.courseid = s.courseid LEFT JOIN springdemand d ON s.courseid = d.courseid"
+            cursor.execute(overall_avg_query)
+
+            row = cursor.fetchone()
+            overall_avg_demand = row[0]
+
+            avg_by_dept_query = "SELECT s.deptname, AVG(d.coursedemand) FROM courserecs c LEFT JOIN springcourses s ON c.courseid = s.courseid LEFT JOIN springdemand d ON s.courseid = d.courseid GROUP BY s.deptname"
+            cursor.execute(avg_by_dept_query)
+
+            avg_demand_by_dept = cursor.fetchall()
+
+            count_query = "SELECT s.deptname, COUNT(*) FROM courserecs c LEFT JOIN springcourses s ON c.courseid = s.courseid LEFT JOIN springdemand d ON s.courseid = d.courseid GROUP BY s.deptname"
+            cursor.execute(count_query)
+
+            course_count_by_dept = cursor.fetchall() # Formatted as a list of tuples (department name, count).
+
+            high_demand_query = "SELECT c.courseid, s.fullcode, s.title, s.description, d.coursedemand, c.similarity FROM courserecs c LEFT JOIN springcourses s ON c.courseid = s.courseid LEFT JOIN springdemand d ON s.courseid = d.courseid WHERE d.coursedemand > (SELECT AVG(d.coursedemand) FROM courserecs c LEFT JOIN springdemand d)"
+            cursor.execute(high_demand_query)
+
+            high_demand_courses = cursor.fetchall()
+
+            low_demand_query = "SELECT c.courseid, s.fullcode, s.title, s.description, d.coursedemand, c.similarity FROM courserecs c LEFT JOIN springcourses s ON c.courseid = s.courseid LEFT JOIN springdemand d ON s.courseid = d.courseid WHERE d.coursedemand < (SELECT AVG(d.coursedemand) FROM courserecs c LEFT JOIN springdemand d)"
+            cursor.execute(low_demand_query)
+
+            low_demand_courses = cursor.fetchall()
+
+            return Recommendation(sorted_by_similarity, sorted_by_demand, overall_avg_demand, avg_demand_by_dept, course_count_by_dept, high_demand_courses, low_demand_courses)
 
 if __name__ == "__main__":
 
@@ -142,4 +198,7 @@ if __name__ == "__main__":
     m = create_tfidf(d)
     n = create_cosine_matrix(m)
 
-    print(get_recommendations("Algorithms", n))
+    recs = get_recommendations('Algorithms', n)
+    build_rec_table(recs)
+    new_r = join_and_update_recs()
+    print(new_r.get_avg_dept())
